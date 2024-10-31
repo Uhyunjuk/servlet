824ac8504895580ff3eb751da0f52f9b6ddf606a
Uhyunjuk <toearthgold@gmail.com>
2024-10-31 20:53:04 +0900

    :tada: Feat: 서블릿으로 파라미터 요청, 응답 body에 메세지 남기기

---
+++ src/test/java/hello/servlet_study/ServletStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.servlet_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
